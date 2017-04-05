b421bf0f55dccfdd5fc7b591b9f886b23b046d9a
alexbai94 <alexbai94@gmail.com>
2017-04-04 17:13:56 -0700

    Completed front end revision to use rest

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -40,6 +40,7 @@ class Application extends CI_Controller {
 			$this->data['content'] = $this->parser->parse($this->data['pagebody'], $this->data, true);
 
         // integrate any needed CSS framework & components
+
         $this->data['caboose_styles'] = $this->caboose->styles();
         $this->data['caboose_scripts'] = $this->caboose->scripts();
         $this->data['caboose_trailings'] = $this->caboose->trailings();
